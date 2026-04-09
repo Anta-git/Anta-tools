675127b53032a55e37d7ebd34ea494ffefa3c624
Anta-git <j0siddy@gmail.com>
2026-04-09 12:48:03 -0500

    Remove mentions of a footer

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@
  */
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-//todo: import Footer from './components/Footer';
 
 import Home from "./pages/Home";
 import Games from "./pages/games/Games";
